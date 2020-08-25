fc1185a71721ac07c54acc4999c9ebe92ebd0725
Yaseen <al.hashimi.yaseen@hotmail.com>
2020-08-25 14:13:04 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Setting tags as variables
+var menuBtn = document.querySelector(".menu-btn");
+var menu = document.querySelector(".menu");
+var menuNav = document.querySelector(".menu-nav");
+var menuBranding = document.querySelector(".menu-branding");
+var navItems = document.querySelectorAll(".nav-item");
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+// Menu button
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+    showMenu = false;
+  }
+}
